3e15f6dfc46fccac5dc6826836ea7f81e52f6d67
KimberlyJBird <kimberly.j.bird@gmail.com>
2018-03-01 11:09:57 -0600

    ready for migration

--- job-tracker/Data/ApplicationDbContext.cs
+++ job-tracker/Data/ApplicationDbContext.cs
@@ -26,7 +26,7 @@ namespace job_tracker.Data
             // Customize the ASP.NET Identity model and override the defaults if needed.
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
-        
+            base.OnModelCreating(builder);
         }
     }
 }
