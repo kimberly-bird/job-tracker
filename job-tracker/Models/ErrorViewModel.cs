261d08c7274d62b98cd4dcc16a4c9bc09652cdcd
KimberlyJBird <kimberly.j.bird@gmail.com>
2018-03-01 10:53:08 -0600

    initial commit

---
+++ job-tracker/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace job_tracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
